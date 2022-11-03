5e83d9cde9f7833e49515f9e3ebf8d3b96e0df33
Maxime Ortuno <maxime.ortuno@hotmail.fr>
2022-11-03 19:42:55 +0100

    création d'objet dynamique avec javascript

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,33 @@
+let products = []
+
+
 fetch("http://localhost:3000/api/products/")
 
    .then(function(res) {
     if (res.ok) {
       return res.json();
     }
+    
   })
-  .then(function(value) {
-    console.log(value);
-  })
+
+  .then(data => {
+    products = data;
+    console.log(products)
+   })
   .catch(function(err) {
     // Une erreur est survenue
   });
+let b = document.body;
+let newP = document.createElement('p');
+let newTexte = document.createTextNode('texte écrit en javascript')
+newP.textContent = 'Paragraphe créé et inséré grâce a Javascript';
+
+b.prepend(newP);
+b.append(newTexte);
+
+const newEltdiv = document.createElement("div");
+const newElta = document.createElement("a");
+let elt = document.getElementById("items");
+
+elt.appendChild(newEltdiv);
+newEltdiv.appendChild(newElta);
